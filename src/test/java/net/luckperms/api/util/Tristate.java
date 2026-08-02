837158c431fee8e1c2dee859fcf9b5e95b4437b8
Levosilimo <lev.sylin@gmail.com>
2026-08-02 07:13:56 +0200

    chore(license): add SPDX MIT headers to all .java files (lib-24)
    Per lib-24 audit, 83 of 107 .java files lacked SPDX MIT headers. The
    pre-commit hook blocks commits with bare files; any future edit to a
    bare file would have hard-failed the gate.
    
    Added the standard SPDX block to all 83 files:
      /*
       * SPDX-License-Identifier: MIT
       * Copyright (c) 2025 Levosilimo
       * https://github.com/Levosilimo/Everlasting-Skins
       */
    
    All tests pass. Aislop clean. Zero new dependencies.

--- src/test/java/net/luckperms/api/util/Tristate.java
+++ src/test/java/net/luckperms/api/util/Tristate.java
@@ -1,3 +1,10 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2025 Levosilimo
+ * https://github.com/Levosilimo/Everlasting-Skins
+ */
+
+
 package net.luckperms.api.util;
 
 public enum Tristate {
